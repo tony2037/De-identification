aae95e5f4cf1e957d633ad1dc3a09de0e7c203b7
ztex <ztex030640417@gmail.com>
2019-03-28 22:02:27 +0800

    Fix Have to reshape to 2d input to fit sklearn LinearRegression

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,5 +1,7 @@
 import json
+from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
+import numpy as np
 
 def read_train_data(file_path):
 	print('Read train data ' + '-' * 10)
@@ -10,11 +12,29 @@ def read_train_data(file_path):
 	for i in data:
 		features.append(i['features'])
 		labels.append(i['labels'])
+		for k in range(0, (128 - len(features[-1]))):
+			features[-1].append([0.]* len(features[-1][0]))
+		for k in range(0, (128 - len(labels[-1]))):
+			labels[-1].append(0.)
 	assert(len(features) == len(labels))
 	return features, labels
 
-def logistic_model():
-	None
+def logistic_model(features, labels):
+	X = []
+	Y = []
+	for i, j in zip(features, labels):
+		X.append(np.array(i, dtype=np.float32))
+		Y.append(np.array(j, dtype=np.float32))
+	X = np.asarray(X, dtype=np.float32)
+	Y = np.asarray(Y, dtype=np.float32)
+	print(X.shape)
+	print(Y.shape)
+	X = X.reshape((X.shape[0], X.shape[1] * X.shape[2])) # Because sklearn only accept 2d input data
+	print(X.shape)
+	model = LinearRegression()
+	model.fit(X, Y)
+	return None
 
 if __name__ == '__main__':
 	features, labels = read_train_data('data/train.json')
+	model = logistic_model(features, labels)
