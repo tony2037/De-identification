e2cff94e8273c8e9faea608183e1db8ac6eb0e5f
ztex <ztex030640417@gmail.com>
2019-03-28 13:26:41 +0800

    Feat Save training data as json

--- preprocess.py
+++ preprocess.py
@@ -28,34 +28,50 @@ def read_labels(file_path = ''):
     with open(file_path, newline = '') as f:
         raw = csv.reader(f)
         for i in raw:
-            labels.append(i)
-            print(i)
+            labels.append([float(j) for j in i])
+            labels[-1]
     return labels
 
-def check_length(features, labels):
+def check_length(features, labels, tokens):
     x = []
     y = []
-    for i, j in zip(features, labels):
+    z = []
+    for i, j, k in zip(features, labels, tokens):
         print('feat len : %s' % str(len(i)))
         print('label len : %s' % str(len(j)))
         if(len(i) - 2 == len(j)):
             print('Append a right data')
             x.append(i)
             y.append(j)
+            z.append(k)
     print('Now is having %s verified data' % str(len(x)))
-    return x, y
+    return x, y, z
 
 def data2TFRecord(features, labels, file_path):
     writer = tf.python_io.TFRecordWriter(file_path)
-    for x, y in zip(features, labels):
+    features_tf = []
+    for i in features:
+        features_tf.append(tf.train.FloatList(value=[i]))
+    for x, y in zip(features_tf, labels):
+        print(x)
+        print(y)
         example = tf.train.Example(features=tf.train.Features(feature={\
-                'label': tf.train.Feature(int_64_list=tf.train.Int64List(value=[y])),\
-                'feature': tf.train.Feature(float_64_list=tf.train.Float64List(value=[x]))\
+                'label': tf.train.Feature(float_list=tf.train.FloatList(value=y)),\
+                'feature': tf.train.Feature(float_list=tf.train.FloatList(value=[x]))\
             }))
         writer.write(example.SerializeToString())
     writer.close()
 
+def data2json(features, labels, tokens, file_path):
+    data = []
+    for i, j, k in zip(features, labels, tokens):
+        data.append({'features': i, 'labels': j, 'tokens': k})
+    with open(file_path, 'w') as f:
+        json.dump(data, f, ensure_ascii = False)
+
 if __name__ == '__main__':
-    features, tokens = read_features(glob_condition = 'data/features/*.json')
-    labels = read_labels(file_path = 'data/label.csv')
-    features, labels = check_length(features, labels)
+	features, tokens = read_features(glob_condition = 'data/features/*.json')
+	labels = read_labels(file_path = 'data/label.csv')
+	features, labels, tokens = check_length(features, labels, tokens)
+	data2json(features, labels, tokens, 'data/train.json')
+        # data2TFRecord(features, labels, 'data/train.tfrecords')
