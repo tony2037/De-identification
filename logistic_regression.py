2cbd520484c9542be7242d86f82f245da44e9b02
ztex <ztex030640417@gmail.com>
2019-03-28 20:53:08 +0800

    Cont Keep on read file

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,10 +1,17 @@
 import json
 
 def read_train_data(file_path):
+	print('Read train data ' + '-' * 10)
+	features = []
+	labels = []
 	with open(file_path, 'r') as f:
 		data = json.loads(f.read())
-	print(len(data))
+	for i in data:
+		features.append(i['features'])
+		labels.append(i['labels'])
+	assert(len(features) == len(labels))
+	return features, labels
 
 
 if __name__ == '__main__':
-	read_train_data('data/train.json')
+	features, labels = read_train_data('data/train.json')
