501746b24b243fb01b795d7c46ada658ca6c313a
ztex <ztex030640417@gmail.com>
2019-04-02 12:18:49 +0800

    Feat Read raw data and going to check the consistency

--- dataset_analysis.py
+++ dataset_analysis.py
@@ -8,11 +8,30 @@ Read the labels from the file
 @filename: the .csv file containing labels
 '''
 def read_labels(filename = 'data/label.csv'):
-    print('read labels ---')
-    labels = []
-    with open(filename, newline = '') as f:
-        raw = csv.reader(f)
-        for i in raw:
-            labels.append(i)
-            print(i)
-    return labels
+	print('read labels ---')
+	labels = []
+	with open(filename, newline = '') as f:
+		raw = csv.reader(f)
+		for i in raw:
+			labels.append(i)
+			print(i)
+	return labels
+
+def read_raw(glob_condition = ''):
+	sentences = []
+	for i in glob(glob_condition):
+		with open(i, 'r') as f:
+			sentences.append(f.read())
+			print(sentences[-1])
+	return sentences
+
+def check_consistency(sentences, labels):
+	print('Check raw data and labels are matched to each other')
+	for i, j in zip(sentences, labels):
+		print(type(i))
+		print(type(j))
+
+if __name__ == '__main__':
+	labels = read_labels('data/label.csv')
+	sentences = read_raw('data/raw/*.sentence')
+	check_consistency(sentences, labels)
